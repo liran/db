af06aa3274deb43cc6a05cf6911c40ba613bf748
liran <857271370@qq.com>
2022-11-15 21:54:28 +0800

    Add conflict test

--- db_test.go
+++ db_test.go
@@ -1,10 +1,12 @@
 package db
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func TestAll(t *testing.T) {
@@ -115,4 +117,34 @@ func TestNotFound(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestConflict(t *testing.T) {
+	db, err := New("/tmp/db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for i := 0; i < 5; i++ {
+		go func(index int) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				db.Txn(func(txn *Txn) error {
+					val, _ := txn.Get("kk")
+					log.Printf("[%d] %s", index, val)
+					return txn.Set("kk", index)
+				})
+			}
+		}(i)
+	}
+	<-ctx.Done()
 }
